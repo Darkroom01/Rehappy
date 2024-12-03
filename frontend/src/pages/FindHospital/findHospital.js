93383bef11bfad0926ba5a0d4cb1e12415200e76
Woojeong Hyun <woo6142@naver.com>
2024-12-04 05:06:27 +0900

    응급모드 구현

--- frontend/src/pages/FindHospital/findHospital.js
+++ frontend/src/pages/FindHospital/findHospital.js
@@ -1,5 +1,5 @@
 import {
-    ContentsContainer, HospitalInfo, Hospitals,
+    ContentsContainer, EmergencyButton, HospitalInfo, Hospitals,
     Logo,
     LogoWrapper,
     PlacesContainer,
@@ -20,11 +20,11 @@ export default function FindHospital() {
     const API_KEY = process.env.REACT_APP_GOOGLE_PLACES_API_KEY;
     const libraries = ['places'];
     const [hospitals, setHospitals] = useState([]);
+
     const [mapCenter, setMapCenter] = useState({ lat: 37.500967, lng: 126.993653 }); // 초기 중심점
     const [mapCenterCountry, setMapCenterCountry] = useState('');
     const [selectedHospital, setSelectedHospital] = useState(null); // 선택된 병원 정보
     const [keyword, setKeyword] = useState("병원"); // 초기값 "병원"
-    const [mapInstance, setMapInstance] = useState(null); // Google Map 인스턴스
     const [isEmergency, setIsEmergency] = useState(false); // 응급실 현황 검색: true, 일반 병원 검색: false
 
     const [hospitalData, setHospitalData] = useState([]);
@@ -77,39 +77,76 @@ export default function FindHospital() {
         fetchLocationInfo();
     }, [mapCenter]);
 
+    const fetchLatLng = async (hospitalName) => {
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+            hospitalName
+        )}&key=${API_KEY}`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (data.status === "OK") {
+                const location = data.results[0].geometry.location;
+                console.log('location:',location);
+                return {
+                    lat: location.lat,
+                    lng: location.lng,
+                };
+            } else {
+                console.error(`Google Maps API 에러: ${data.status}`);
+                return { lat: null, lng: null };
+            }
+
+        } catch (error) {
+            console.error("Google Maps API 요청 실패:", error);
+            return { lat: null, lng: null };
+        }
+    };
 
     // 응급실 가용 병상 수 현황 가져오기
-    useEffect(() => {
-        const fetchData = async () => {
-            const url = `${EMERGENCY_API_URL}?serviceKey=${EMERGENCY_API_KEY}&STAGE1=${mapCenterCountry}&pageNo=1&numOfRows=10`;
+    const fetchEmergencyInfo = async () => {
+        const url = `${EMERGENCY_API_URL}?serviceKey=${EMERGENCY_API_KEY}&STAGE1=${mapCenterCountry}&pageNo=1&numOfRows=10`;
 
-            try {
-                const response = await fetch(url);
-                if (response.ok) {
-                    const xmlText = await response.text(); // XML 데이터를 텍스트로 받아오기
-                    const parser = new DOMParser();
-                    const xml = parser.parseFromString(xmlText, "application/xml"); // XML 파싱
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const xmlText = await response.text(); // XML 데이터를 텍스트로 받아오기
+                const parser = new DOMParser();
+                const xml = parser.parseFromString(xmlText, "application/xml"); // XML 파싱
 
-                    const items = xml.getElementsByTagName("item");
-                    const data = Array.from(items).map((item) => ({
-                        name: item.getElementsByTagName("dutyName")[0]?.textContent || "정보 없음",
-                        beds: item.getElementsByTagName("hvec")[0]?.textContent || "정보 없음",
-                        phone: item.getElementsByTagName("dutyTel3")[0]?.textContent || "정보 없음",
-                    }));
+                const items = xml.getElementsByTagName("item");
+                const data = Array.from(items).map((item) => ({
+                    name: item.getElementsByTagName("dutyName")[0]?.textContent || "정보 없음",
+                    beds: item.getElementsByTagName("hvec")[0]?.textContent || "정보 없음",
+                    phone: item.getElementsByTagName("dutyTel3")[0]?.textContent || "정보 없음",
+                }));
 
-                    setHospitalData(data);
-                } else {
-                    setError(`HTTP 에러: ${response.status}`);
-                }
-            } catch (err) {
-                setError("데이터를 가져오는 중 오류 발생");
-                console.error(err);
-            } finally {
-                setLoading(false);
+                // Google Maps API로 병원 좌표 가져오기
+                const updatedData = await Promise.all(
+                    data.map(async (hospital) => {
+                        const latLng = await fetchLatLng(hospital.name);
+                        return {
+                            ...hospital,
+                            lat: latLng.lat,
+                            lng: latLng.lng,
+                        };
+                    })
+                );
+                setHospitalData(updatedData);
+            } else {
+                setError(`HTTP 에러: ${response.status}`);
             }
-        };
-        fetchData();
+        } catch (err) {
+            setError("데이터를 가져오는 중 오류 발생");
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    useEffect(() => {
+        fetchEmergencyInfo();
     }, [mapCenter]);
 
     // 검색어(keyword)에 따라 데이터 가져오기
@@ -122,16 +159,6 @@ export default function FindHospital() {
         fetchHospitals();
     }, [mapCenter, keyword]); // mapCenter와 keyword 변경 시 API 호출
 
-    // 지도 드래그 후 mapCenter 업데이트
-    const handleDragEnd = () => {
-        if (mapInstance) {
-            const newCenter = mapInstance.getCenter();
-            setMapCenter({
-                lat: newCenter.lat(),
-                lng: newCenter.lng(),
-            });
-        }
-    };
 
     const getColor = (beds) => {
         const numBeds = parseInt(beds, 10); // 문자열을 숫자로 변환
@@ -149,13 +176,12 @@ export default function FindHospital() {
                 <MapComponent
                     libraries={libraries}
                     mapCenter={mapCenter}
-                    setMapInstance={setMapInstance}
-                    handleDragEnd={handleDragEnd}
                     hospitals={hospitals}
                     selectedHospital={selectedHospital}
                     setSelectedHospital={setSelectedHospital}
                     isEmergency={isEmergency}
                     hospitalData={hospitalData}
+                    setMapCenter={setMapCenter}
                 />
                 {/* 좌측 컨테이너 */}
                 <PlacesContainer>
@@ -163,20 +189,29 @@ export default function FindHospital() {
                         <LogoWrapper>
                             <Logo onClick={() => navigate('/')} src='/images/logo.png' alt='리해피 로고' />
                         </LogoWrapper>
-                        <button onClick={() => setIsEmergency(!isEmergency)}>응급실 함보까</button>
+                        <EmergencyButton onClick={() => {
+                            setIsEmergency(!isEmergency);
+                            fetchEmergencyInfo();
+                        }}>
+                            <img style={{height: 'calc(100% - 50px)', width: 'calc(100% - 50px)'}} src='/images/emergencyIcon.png' alt='응급실 찾기 버튼'/>
+                            <p style={{marginTop: '5px', fontSize: '15px', fontWeight: 'bold'}}>응급실 현황</p>
+                        </EmergencyButton>
                         {isEmergency ? (
                             // 응급 상황 병원 검색 모드
                             <>
-                                {hospitalData.map((hospital, index) => (
-                                    <div style={{width: '80%', marginBottom: '20px', fontSize: '18px', lineHeight: '1.2'}} key={index}>
-                                        <div style={{fontWeight: 'bold'}}>{hospital.name}</div>
-                                        <div>
-                                            가용 병상 수:{" "}
-                                            <span style={{color: getColor(hospital.beds)}}>{hospital.beds}</span>
-                                        </div>
-                                        <div>응급실 전화번호: {hospital.phone}</div>
-                                    </div>
-                                ))}
+                                <Hospitals style={{margin: '20px 0'}}>
+                                    {hospitalData.map((hospital, index) => (
+                                        <HospitalInfo onClick={() => setSelectedHospital(hospital)} key={index}>
+                                            <div style={{fontWeight: 'bold'}}>{hospital.name}</div>
+                                            <div>
+                                                가용 병상 수:{" "}
+                                                <span style={{color: getColor(hospital.beds)}}>{hospital.beds}</span>
+                                            </div>
+                                            <div>응급실 전화번호: {hospital.phone}</div>
+                                        </HospitalInfo>
+                                    ))}
+                                </Hospitals>
+
                             </>
                         ) : (
                             // 일반 병원 검색 모드
